dbbc1721024c00b31d67e4a63d21b091a06df129
Kschong0420 <kschong033@gmail.com>
2021-04-18 10:42:43 +0800

    first commit

---
+++ commands/nsfwcreampie.js
@@ -0,0 +1,15 @@
+// Imports Discord.JS and HMtai //
+const Discord = require("discord.js");
+const hmtai = require("hmtai");
+
+module.exports = {
+    name: 'nsfwcreampie',
+    aliases: ['ncreampie'],
+    async execute(cilent, message, args, Discord) {
+        if(!message.channel.nsfw){ message.channel.send("This command can only be used in channels marked nsfw."); return; }
+        const embed = new Discord.MessageEmbed()
+
+        .setImage(hmtai.nsfw.creampie())
+        message.channel.send(embed)
+    }
+}
---
+++ commands/nsfwcum.js
@@ -0,0 +1,15 @@
+// Imports Discord.JS and HMtai //
+const Discord = require("discord.js");
+const hmtai = require("hmtai");
+
+module.exports = {
+    name: 'nsfwcum',
+    aliases: ['ncum'],
+    async execute(cilent, message, args, Discord) {
+        if(!message.channel.nsfw){ message.channel.send("This command can only be used in channels marked nsfw."); return; }
+        const embed = new Discord.MessageEmbed()
+
+        .setImage(hmtai.nsfw.cum())
+        message.channel.send(embed)
+    }
+}
