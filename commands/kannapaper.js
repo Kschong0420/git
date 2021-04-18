0f307bfc9294b50bcd2de008f6df40e1780e4524
Kschong0420 <kschong033@gmail.com>
2021-04-18 14:05:26 +0800

    kp change to neko api and igfd npm removed

--- commands/kannapaper.js
+++ commands/kannapaper.js
@@ -1,15 +1,27 @@
-const Discord = require('discord.js')
-const discordImgGen = require('image-generation-for-discord')//npm i image-generation-for-discord
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
     name: "kannapaper",
     aliases: ["kp"],
-    async execute(client, message, args) {
-        const text = args.join(" ")
-        if (!text) return message.channel.send("Please write something on Kanna's paper")
+
+     async execute(client, message, args) {
+
+        let text = args.join(" ");
+
+        if(!text){
+            return message.channel.send("Please write something on Kanna's paper.");
+        }
+
         if (text.length > 48) return message.channel.send("You write too many text until Kanna cannot write all in her paper.")
 
-        const img = await discordImgGen.kannapaper(text)
-        message.channel.send(new Discord.MessageAttachment(img, "kannapaper.png"))
-    }
-}
+        try {
+            let res = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=kannagen&text=${text}`));
+            let json = await res.json();
+
+           message.channel.send(json.message);
+           } catch (err) {
+               return message.channel.send('An error occured.')
+           }
+       }
+};
