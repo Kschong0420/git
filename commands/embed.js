d70e7270b4d0815c48c8f70039832895abc14418
Kschong0420 <kschong033@gmail.com>
2021-05-21 23:44:55 +0800

    first commit

---
+++ commands/embed.js
@@ -0,0 +1,71 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: 'embed',
+
+  async execute(client, message, args) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Unknown Command.")
+
+    let questions = {
+        firstQuestion: "What do you want to be the embed's title? Type the title into the chat now!",
+        secondQuestion: "Type the description that you want in the embed!",
+        thirdQuestion: "Type the color you want to be on the embed! It can be a color like red or a hex code.",
+        fourthQuestion: "What do you want its footer to be?",
+    }
+        message.channel.send('This will continue in DMs.')
+        message.author.send(questions.firstQuestion).then(msg => {
+            const filter1 = m => m.author.id === message.author.id
+            msg.channel.awaitMessages(filter1, {
+                time: 5 * 60000,
+                max: 1
+            }).then(messages => {
+                let msg1 = messages.first().content
+                if(msg1.toLowerCase() === "cancel") return message.author.send("Process cancelled.")
+                message.author.send(questions.secondQuestion).then(msg => {
+                    const filter1 = m => m.author.id === message.author.id
+                    msg.channel.awaitMessages(filter1, {
+                        time: 5 * 60000,
+                        max: 1
+                    }).then(messages => {
+                        let msg2 = messages.first().content
+                        if(msg2.toLowerCase() === "cancel") return message.author.send("Process cancelled.")
+                        message.author.send(questions.thirdQuestion).then(msg => {
+                            const filter1 = m => m.author.id === message.author.id
+                            msg.channel.awaitMessages(filter1, {
+                                time: 5 * 60000,
+                                max: 1
+                            }).then(messages => {
+                                let msg3 = messages.first().content.toUpperCase()
+                                if(msg3.toLowerCase() === "cancel") return message.author.send("Process cancelled.")
+                                message.author.send(questions.fourthQuestion).then(msg => {
+                                    const filter1 = m => m.author.id === message.author.id
+                                    msg.channel.awaitMessages(filter1, {
+                                        time: 5 * 60000,
+                                        max: 1
+                                    }).then(messages => {
+                                        let msg4 = messages.first().content
+                                        if(msg4.toLowerCase() === "cancel") return message.author.send("Process cancelled.")
+                                        message.author.send("Embed created.")
+                                            
+                                                    message.channel.send(
+                                                        new discord.MessageEmbed()
+                                                            .setTitle(msg1)
+                                                            .setDescription(msg2)
+                                                            .setColor(msg3)
+                                                            .setFooter(msg4)
+                                                    )
+                                            
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            
+     
+
+     
+  }
+}
