8ae35190c417d41109295edf07aa275b4466624b
Kschong0420 <kschong033@gmail.com>
2021-05-23 01:53:51 +0800

    add cooldown system

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,5 +1,6 @@
 const { Message } = require('discord.js')
 
+const cooldowns = new Map();
 require('dotenv').config()
 module.exports = (Discord, client, message) => {
   const prefix = process.env.PREFIX 
@@ -9,8 +10,34 @@ module.exports = (Discord, client, message) => {
   const cmd = args.shift().toLowerCase()
 
   const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+  
+  if(!cooldowns.has(command.name)){
+    cooldowns.set(command.name, new Discord.Collection());
+  }
 
-  if (command) command.execute(client, message, args, Discord)
+  const current_time = Date.now();
+  const time_stamps = cooldowns.get(command.name)
+  const cooldown_amount = (command.cooldown) * 1000;
+
+  if(time_stamps.has(message.author.id)){
+    const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+
+    if(current_time < expiration_time){
+      const time_left = (expiration_time - current_time) / 1000;
+
+      return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name} command.`)
+    }
+  }
+
+  time_stamps.set(message.author.id, current_time)
+  setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
+
+  try{
+    command.execute(client, message, args, Discord);
+  } catch (err) {
+    message.reply("There was an error trying execute this command!");
+    console.log(err)
+  }
 }
 
 ///note
