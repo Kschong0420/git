7043ebed6744d6df69fce624b97daf01a4449b66
Kschong0420 <kschong033@gmail.com>
2021-07-14 03:41:44 +0800

    update

--- commands/Fun/ship.js
+++ commands/Fun/ship.js
@@ -1,29 +1,45 @@
-const Discord = require("discord.js")
+const block = "⬛";
+const heart = ":red_square:";//put your own block emoji if you have smth
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "ship",
-    cooldown: 5,
-    description: 'Combine two peoples name.',
-    usage: 'ship <user1> <user2>',
     category: 'Fun',
+    usage: "ship <username> \`or\` ship <user1> <user2>",
+    cooldown: 5,
+    description: 'See the ship meter between you and the user you mentioned or two users you mentioned.',
+
     async execute(client, message, args) {
-        if (!args[0]) return message.lineReply("Please mention someone!")
-        if (!args[1]) return message.lineReply("Please menton someone else!")
-        if (args[0] || args[1]) {
-            var FirstUser = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-            var SecondUser = message.mentions.members.first(-1) || message.guild.members.cache.get(args[1])
+        const user = message.mentions.users.first();
+        if (!user) return message.channel.send(`Please specify a user you want to ship with.`)
+        if (user && user.id === message.author.id) {
+            return message.lineReplyNoMention("Bruh you want to ship yourself XD.")
+        }
+        if (message.mentions.users.size < 2) {
+            let loveEmbed = new MessageEmbed()
+                .setColor('dd2e44')
+                .setTitle('Shipping...')
+                .setDescription(`Shipped ${message.author} and ${user}!`)
+                .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.author.displayAvatarURL({ dynamic: false, format: "png" })}&user2=${user.displayAvatarURL({ dynamic: false, format: "png" })}`)
+                .addField(`**Ship Meter**`, ship())
 
-            if (!FirstUser) return message.lineReply(`Unknown username: **${args[0]}**!`)
-            if (!SecondUser) return message.lineReply(`Unknown username: **${args[1]}**!`)
-
-            if (FirstUser || SecondUser) {
-                const FirstUserSliced = FirstUser.user.username.slice(0, FirstUser.user.username.length / 2)
-                const SecondUserSliced = SecondUser.map(user => { return user.user.username.slice(user.user.username.length / 2) })
-                const SecondUserName = SecondUser.map(user => { return user.user.username })
-
-                message.lineReplyNoMention(`${FirstUser.user.username} 💕 ${SecondUserName} = **${FirstUserSliced}${SecondUserSliced}**`)
-            }
-
+            return message.lineReplyNoMention(loveEmbed)
+        } else if (message.mentions.users.size > 1) {
+            let luv = new MessageEmbed()
+                .setColor('dd2e44')
+                .setTitle('Shipping...')
+                .setDescription(`Shipped ${message.mentions.users.first()} and ${message.mentions.users.last()}!`)
+                .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.mentions.users.first().displayAvatarURL({ dynamic: false, format: "png" })}&user2=${message.mentions.users.last().displayAvatarURL({ dynamic: false, format: "png" })}`)
+                .addField(`**Ship Meter**`, ship())
+            message.lineReplyNoMention(luv)
         }
     }
+}
+
+function ship() {
+    const hearts = Math.floor(Math.random() * 110) + 0;
+    const hearte = (hearts / 10)
+
+    const str = `${heart.repeat(hearte)}${block.repeat(11 - hearte)} ${hearts}%`;
+    return str;
 }
