71b5be1f59d69ccd1beb3eb1cfe44c0a628abf31
Kschong0420 <kschong033@gmail.com>
2021-07-19 20:19:15 +0800

    update detect

--- commands/Action/pout.js
+++ commands/Action/pout.js
@@ -11,16 +11,30 @@ module.exports = {
         try {
             const check = message.mentions.users.first()
             const author = message.author.username;
-            const user = message.mentions.users.first().username
-            if (!user) return message.lineReplyNoMention("Please mention someone.")
-            if (check.id === message.author.id) return message.lineReplyNoMention('Even though you can let yourself pout but it\'s bored, try let someone pout.')
             const res = await fetch('https://shiro.gg/api/images/pout');
             const img = (await res.json()).url;
-            const pout = new Discord.MessageEmbed()
-                .setTitle(`**${author}** let **${user}** pout! (●´ϖ\`●)`, true)
-                .setImage(img)
-                .setColor('#00FFFF')
-            message.lineReplyNoMention(pout);
+            if (check) {
+                if (check.id === message.author.id) {
+                    const pout = new Discord.MessageEmbed()
+                        .setTitle(`**${author}** is pouting! (●´ϖ\`●)`, true)
+                        .setImage(img)
+                        .setColor('#00FFFF')
+                    message.lineReplyNoMention(pout)
+                } else {
+                    const user = message.mentions.users.first().username
+                    const pout = new Discord.MessageEmbed()
+                        .setTitle(`**${author}** let **${user}** pout! (●´ϖ\`●)`, true)
+                        .setImage(img)
+                        .setColor('#00FFFF')
+                    message.lineReplyNoMention(pout)
+                }
+            } if (!check) {
+                const pout = new Discord.MessageEmbed()
+                    .setTitle(`**${author}** is pouting! (●´ϖ\`●)`, true)
+                    .setImage(img)
+                    .setColor('#00FFFF')
+                message.lineReplyNoMention(pout)
+            }
         } catch (error) {
             console.log(error);
             return message.lineReplyNoMention('Please mention someone.')
