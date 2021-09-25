1453c83e5175dd6d4bf1c1e736f1342798ca4c94
Kschong0420 <kschong033@gmail.com>
2021-09-26 02:14:43 +0800

    first commit

---
+++ commands/Nsfw/yaoi.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "yaoi",
+    cooldown: 7,
+    description: 'Return a random yaoi picture.',
+    aliases: ['nsfwyaoi'],
+    usage: 'yaoi',
+    category: 'NSFW',
+
+    async execute(client, message, args) {
+        if(!message.channel.nsfw){ message.lineReplyNoMention("This command can only be used in channels marked nsfw."); return; }
+
+        try {
+            let res = await fetch(encodeURI(`https://nekobot.xyz/api/image?type=yaoi`));
+            let json = await res.json();
+
+            const embed = new Discord.MessageEmbed()
+                .setImage(json.message)
+            message.lineReplyNoMention(embed);
+        } catch (err) {
+            return message.lineReplyNoMention('An error occured.')
+        }
+    }
+};
